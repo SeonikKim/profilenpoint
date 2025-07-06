3e8e6fe48683e36b83ae7640f5f54895e6ff6c29
SeonikKim <tjsdlrdl122@naver.com>
2025-07-06 23:54:17 +0900

    feat : 스프링부트 세팅

---
+++ src/main/java/com/seonik/profile/ProfilenpointApplication.java
@@ -0,0 +1,13 @@
+package com.seonik.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilenpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilenpointApplication.class, args);
+	}
+
+}
